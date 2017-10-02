7a70381ec4ac31efb87f21ee02e6e7a7ce0dac1b
Ivan Padavic <ipadavic@gmail.com>
2017-10-02 12:24:01 +0200

    removed test testCase

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -17,13 +17,4 @@ class ClientTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(is_object($client->setBaseUrl("https://api.rentl.io/v2")));
         unset($var);
     }
-
-    public function testGetMyData()
-    {
-        $client = new \Rentlio\Api\Client("9922098e3f8248ae978cb4fe96c193f9");
-        $request = new \Rentlio\Api\Request\GetMyDataRequest();
-        $response = $client->send($request);
-        var_dump(json_decode($response->getBody()->getContents()));
-    }
-
 }
