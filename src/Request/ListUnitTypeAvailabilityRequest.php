87c9a0f10ab2bf52ca90c64e40bc9cba25b53557
Filip Aleksić <filipp.aleksic@gmail.com>
2017-10-10 16:15:07 +0200

    Add ListUnitTypeAvailabilityRequest and test for that class

---
+++ src/Request/ListUnitTypeAvailabilityRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Rentlio\Api\Request;
+
+class ListUnitTypeAvailabilityRequest extends AbstractRequest
+{
+    protected $id;
+    protected $dateFrom;
+    protected $dateTo;
+
+    public function __construct($id, array $headers = [], $body = null, $version = '1.1')
+    {
+        parent::__construct("GET", "/properties/" . $id . "/unit-types", $headers, $body, $version);
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setDateFrom($dateFrom)
+    {
+        $this->dateFrom = $dateFrom;
+    }
+
+    public function setDateTo($dateTo)
+    {
+        $this->dateTo = $dateTo;
+    }
+
+    /**
+    * @return array
+    */
+    public function getQueryParams()
+    {
+        return [
+            'id' => $this->id,
+            'dateFrom' => $this->dateFrom,
+            'dateTo' => $this->dateTo
+        ];
+    }
+}
+?>
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -17,13 +17,17 @@ class ClientTest extends PHPUnit_Framework_TestCase
 
     public function testGetMyData()
     {
-        $client   = new Rentlio\Api\Client("04b028b0ac3b4e5882a0085cba36415f");
-        $request  = new Rentlio\Api\Request\GetMyDataRequest();
-        $response = $client->send($request);
-        var_dump(json_decode($response->getBody()->getContents()));
+        $client   = new Rentlio\Api\Client("6d7b0cbb83174b188d579ad9a2182805");
+        //$request  = new Rentlio\Api\Request\GetMyDataRequest();
+        //$response = $client->send($request);
+        //var_dump(json_decode($response->getBody()->getContents()));
 
-        $request = new Rentlio\Api\Request\ListAllPropertiesRequest();
-        $request->setSortOrder('ASC');
+        //$request = new Rentlio\Api\Request\ListAllPropertiesRequest();
+        //$request->setSortOrder('ASC');
+        //$response = $client->send($request);
+        //var_dump(json_decode($response->getBody()->getContents()));
+
+        $request  = new Rentlio\Api\Request\ListUnitTypeAvailabilityRequest(5797);
         $response = $client->send($request);
         var_dump(json_decode($response->getBody()->getContents()));
     }
