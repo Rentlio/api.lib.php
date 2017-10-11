71560c7d863ebadfa8640ffcb619f7cb46bdf3b1
krivite <kristiana735@gmail.com>
2017-10-11 11:52:23 +0200

    Add test for ListAllPropertiesRequest

---
+++ tests/Request/ListAllPropertiesRequestTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class ListAllPropertiesRequestTest extends PHPUnit_Framework_TestCase
+{
+    public function testRequestDefaultUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllPropertiesRequest();
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/properties', $uri->getPath());
+        $this->assertEquals('order_by=id&order_direction=ASC&page=1&name', $uri->getQuery());
+    }
+
+    public function testRequestSortChangedUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllPropertiesRequest();
+        $request->setName('Moji Apartmani');
+        $request->setSortOrder('DESC');
+        $request->setSortBy('name');
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/properties', $uri->getPath());
+        $this->assertEquals('order_by=name&order_direction=DESC&page=1&name=Moji%20Apartmani', $uri->getQuery());
+    }
+}
