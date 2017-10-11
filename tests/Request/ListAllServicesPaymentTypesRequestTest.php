b72fc2155e5dfdcc12600cdedc878cc356f493a7
krivite <kristiana735@gmail.com>
2017-10-11 12:25:46 +0200

    Add test for ListAllServicesPaymentTypesRequest

---
+++ tests/Request/ListAllServicesPaymentTypesRequestTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class ListAllServicesPaymentTypesRequestTest extends PHPUnit_Framework_TestCase
+{
+    public function testRequestDefaultUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllServicesPaymentTypesRequest();
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/enums/services/payment-types', $uri->getPath());
+        $this->assertEquals('order_by=id&order_direction=ASC&page=1', $uri->getQuery());
+    }
+
+    public function testRequestSortChangedUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllServicesPaymentTypesRequest();
+        $request->setSortOrder('DESC');
+        $request->setSortBy('name');
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/enums/services/payment-types', $uri->getPath());
+        $this->assertEquals('order_by=name&order_direction=DESC&page=1', $uri->getQuery());
+    }
+}
