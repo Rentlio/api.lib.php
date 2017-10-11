407a1c133d4eed986253ede9ecc897b71e5eba09
Filip Aleksić <filipp.aleksic@gmail.com>
2017-10-11 12:19:05 +0200

    Add test for ListAllReservationsRequest

---
+++ tests/Request/ListAllReservationsRequestTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+class ListAllReservationsRequestTest extends PHPUnit_Framework_TestCase
+{
+    public function testRequestDefaultUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllReservationsRequest();
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/reservations', $uri->getPath());
+        $this->assertEquals('order_by=id&order_direction=ASC&page=1&status&unitsId&' .
+                            'propertiesId&bookedAtFrom&bookedAtTo&cancelAtFrom&' .
+                            'cancelAtTo&createdAtFrom&createdAtTo&modifiedAtFrom&modifiedAtTo', $uri->getQuery());
+    }
+
+    public function testRequestSortChangedUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllReservationsRequest();
+        $request->setSortOrder('DESC');
+        $request->setSortBy('name');
+        $request->setStatus(1);
+        $request->setUnitsId('2345');
+        $request->setPropertiesId('1212');
+        $request->setBookedAtFrom('2017-12-13');
+        $request->setBookedAtTo('2017-12-23');
+        $request->setCancelAtFrom('2017-12-24');
+        $request->setCancelAtTo('2017-12-26');
+        $request->setCreatedAtFrom('2017-11-24');
+        $request->setCreatedAtTo('2017-11-30');
+        $request->setModifiedAtFrom('2017-12-29');
+        $request->setModifiedAtTo('2017-12-30');
+
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/reservations', $uri->getPath());
+        $this->assertEquals('order_by=name&order_direction=DESC&page=1&status=1&unitsId=2345&' .
+        'propertiesId=1212&bookedAtFrom=2017-12-13&bookedAtTo=2017-12-23&cancelAtFrom=2017-12-24&' .
+        'cancelAtTo=2017-12-26&createdAtFrom=2017-11-24&createdAtTo=2017-11-30' .
+        '&modifiedAtFrom=2017-12-29&modifiedAtTo=2017-12-30', $uri->getQuery());
+    }
+
+}
