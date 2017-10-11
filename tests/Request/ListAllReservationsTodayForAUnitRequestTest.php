0e4d07249e42a5c807764a8f3d6087e2eb7a4d74
krivite <kristiana735@gmail.com>
2017-10-11 12:21:47 +0200

    Add test for ListAllReservationsTodayForAUnitRequest

---
+++ tests/Request/ListAllReservationsTodayForAUnitRequestTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class ListAllReservationsTodayForAUnitRequest extends PHPUnit_Framework_TestCase
+{
+    public function testRequestDefaultUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllReservationsTodayForAUnitRequest(2);
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/units/2/reservations/today', $uri->getPath());
+        $this->assertEquals('order_by=id&order_direction=ASC&page=1', $uri->getQuery());
+    }
+
+    public function testRequestSortChangedUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllReservationsTodayForAUnitRequest(1);
+        $request->setSortOrder('DESC');
+        $request->setSortBy('arrivalDate');
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/units/1/reservations/today', $uri->getPath());
+        $this->assertEquals('order_by=arrivalDate&order_direction=DESC&page=1', $uri->getQuery());
+    }
+}
