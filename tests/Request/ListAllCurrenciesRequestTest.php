75ebe5fca61beadff746ce50e31ba721196afb25
Filip Aleksić <filipp.aleksic@gmail.com>
2017-10-11 11:33:09 +0200

    Add test for ListAllCurrenciesRequest

---
+++ tests/Request/ListAllCurrenciesRequestTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class ListAllCurrenciesRequestTest extends PHPUnit_Framework_TestCase
+{
+    public function testRequestDefaultUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllCurrenciesRequest();
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/enums/currencies', $uri->getPath());
+        $this->assertEquals('order_by=id&order_direction=ASC&page=1', $uri->getQuery());
+    }
+
+    public function testRequestSortChangedUri()
+    {
+        $request = new \Rentlio\Api\Request\ListAllCurrenciesRequest();
+        $request->setSortOrder('DESC');
+        $request->setSortBy('name');
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/enums/currencies', $uri->getPath());
+        $this->assertEquals('order_by=name&order_direction=DESC&page=1', $uri->getQuery());
+    }
+}
