b0147bd3e5d2e995abc71ff4c642af0bc9362a91
Lovro Predovan <lovro.predovan@gmail.com>
2018-07-02 12:15:48 +0200

    Adding support for endpoint that lists properties invoices

---
+++ src/Request/GetInvoicesByProperty.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rentlio\Api\Request;
+
+/**
+ * Class GetInvoicesByProperty
+ * @package Rentlio\Api\Request
+ *
+ * GET Request for listing invoices associated with property
+ * https://docs.rentl.io/#invoices-get-invoices-by-property
+ */
+class GetInvoicesByProperty extends AbstractRequest
+{
+    public function __construct($id)
+    {
+        parent::__construct("GET", "/properties/" . $id . "/invoices");
+    }
+
+    /**
+     * @return array
+     */
+    public function getQueryParams()
+    {
+        return [];
+    }
+}
---
+++ tests/Request/GetInvoicesByPropertyTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class GetInvoicesByPropertyTest extends PHPUnit_Framework_TestCase
+{
+    public function testRequestDefaultUri()
+    {
+        $request = new \Rentlio\Api\Request\GetInvoicesByProperty(1);
+        $uri     = $request->getUri();
+
+        $this->assertEquals('/properties/1/invoices', $uri->getPath());
+        $this->assertEquals('order_by=id&order_direction=ASC&page=1', $uri->getQuery());
+    }
+
+    public function testRequestSortChangedUri()
+    {
+        $request = new \Rentlio\Api\Request\GetInvoicesByProperty(1);
+        $request
+            ->setSortOrder('DESC')
+            ->setSortBy('name');
+        $uri = $request->getUri();
+
+        $this->assertEquals('/properties/1/invoices', $uri->getPath());
+        $this->assertEquals('order_by=name&order_direction=DESC&page=1', $uri->getQuery());
+    }
+
+}
