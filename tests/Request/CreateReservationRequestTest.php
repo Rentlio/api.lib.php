44cd2febffee8fca91852e7cbc1ae45ab14d1ead
Lovro Predovan <lovro.predovan@gmail.com>
2018-07-03 11:12:52 +0200

    Adjusting tests

--- tests/Request/CreateReservationRequestTest.php
+++ tests/Request/CreateReservationRequestTest.php
@@ -17,12 +17,12 @@ class CreateReservationRequestTest extends PHPUnit_Framework_TestCase
         $requestBody = json_encode($request);
         $this->assertJson($requestBody);
 
-        $reservation = new \Rentlio\Api\Request\Data\Reservation(1, '1530537657', '1530537659');
+        $reservation = new \Rentlio\Api\Request\Data\Reservation(1, '1530537657', '1530537659', 1, 2);
         $request->setReservation($reservation);
 
         $requestBody = json_encode($request);
         $this->assertJson($requestBody);
-        $this->assertEquals('{"unitTypeId":1,"dateFrom":"1530537657","dateTo":"1530537659"}', $requestBody);
+        $this->assertEquals('{"unitTypeId":1,"dateFrom":"1530537657","dateTo":"1530537659","persons":2,"rooms":1}', $requestBody);
 
         $reservation->setEmail('doe@rentl.io');
         $reservation->setNote('Testing request');
